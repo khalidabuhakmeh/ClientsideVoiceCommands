fb2af4981918b219483a9e441c2dca8ce9001d08
Khalid Abuhakmeh <khalidabuhakmeh@gmail.com>
2021-08-02 14:24:07 -0400

    Initial Commit

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Jarvis.Pages
+{
+    public class Index : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
